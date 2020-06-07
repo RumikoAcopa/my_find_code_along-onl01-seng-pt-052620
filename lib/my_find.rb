affe13556f5e44d560806dba7e640b7504b078a4
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 03:38:47 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    i = i + 1
+    if yield(collection[i])
+    return collection[i]
   end
-collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
-
+  i = i + 1
 end
